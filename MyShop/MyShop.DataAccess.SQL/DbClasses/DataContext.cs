7d0a7985b6c2ff56362e39be7906ff6c8c171ece
krishpatil2911 <krishpatil2911@gmail.com>
2021-12-27 19:48:01 +0530

    implemented basket model

--- MyShop/MyShop.DataAccess.SQL/DbClasses/DataContext.cs
+++ MyShop/MyShop.DataAccess.SQL/DbClasses/DataContext.cs
@@ -18,5 +18,9 @@ namespace MyShop.DataAccess.SQL.DbClasses
         public DbSet<Product> Products { get; set; }
 
         public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        public DbSet<Basket> Baskets { get; set; }
+
+        public DbSet<BasketItem> BasketItems { get; set; }
     }
 }
